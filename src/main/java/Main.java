68b85a55b1972399c025d30990a2dae608078676
tatar <tatarrski_dmitry@mail.ru>
2023-07-04 16:42:58 +0300

    task 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,12 @@
-import task2.ListNode;
-import task3.Solution;
+import task4.Solution;
 
 // the class to check results
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
-        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
-        System.out.println(solution.lengthOfLongestSubstring(" "));
-        System.out.println(solution.lengthOfLongestSubstring("abc"));
+        System.out.println(solution.findMedianSortedArrays(new int[]{}, new int[]{1,2,3}));
+        System.out.println(5 / 2); // 5 / 2 == 2
 
     }
 }
---
+++ src/main/java/task4/Solution.java
@@ -0,0 +1,45 @@
+package task4;
+
+public class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length == 0)
+            return median(nums2);
+        else if (nums2.length == 0)
+            return median(nums1);
+
+        int[] mergedArray = mergeArrays(nums1, nums2);
+        return median(mergedArray);
+    }
+
+    private int[] mergeArrays(int[] nums1, int[] nums2) {
+        int[] mergedArray = new int[nums1.length + nums2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                mergedArray[k++] = nums1[i++];
+            } else {
+                mergedArray[k++] = nums2[j++];
+            }
+        }
+
+        while (i < nums1.length) {
+            mergedArray[k++] = nums1[i++];
+        }
+
+        while (j < nums2.length) {
+            mergedArray[k++] = nums2[j++];
+        }
+
+        return mergedArray;
+    }
+
+    private double median(int[] mergedArray) {
+        int mid = mergedArray.length / 2;
+
+        return mergedArray.length % 2 == 1
+                ? mergedArray[mid]
+                : (mergedArray[mid - 1] + mergedArray[mid]) / 2.0;
+    }
+}
