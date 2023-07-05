96a9eb45cf4cf42f8885b525698100e98518fd03
tatar <tatarrski_dmitry@mail.ru>
2023-07-05 15:48:38 +0300

    task5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,11 @@
-import task4.Solution;
+import task5.Solution;
 
 // the class to check results
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.findMedianSortedArrays(new int[]{}, new int[]{1,2,3}));
-        System.out.println(5 / 2); // 5 / 2 == 2
+        System.out.println(solution.longestPalindrome("bbbbbfedaaaaaaabbbbaaaaaa"));
 
     }
 }
---
+++ src/main/java/task5/Solution.java
@@ -0,0 +1,34 @@
+package task5;
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        int start = 0;
+        int maxLength = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+    private static int expandAroundCenter(String str, int left, int right) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
