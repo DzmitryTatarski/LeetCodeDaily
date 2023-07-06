f1cdece3093115875f8fe44eecf0d0e3897e23d3
tatar <tatarrski_dmitry@mail.ru>
2023-07-06 20:21:50 +0300

    task 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
-import task5.Solution;
+import task6.Solution;
 
 // the class to check results
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(14 / 4);
         Solution solution = new Solution();
-        System.out.println(solution.longestPalindrome("bbbbbfedaaaaaaabbbbaaaaaa"));
+        System.out.println(solution.convert("PAYPALISHIRING", 3));
 
     }
+
+
 }
---
+++ src/main/java/task6/Solution.java
@@ -0,0 +1,26 @@
+package task6;
+
+public class Solution {
+
+    public String convert(String s, int numRows){
+        if (s.length() <= 1 || numRows <= 1)
+            return s;
+        int indexLetter = 0;
+        boolean b = false;
+        StringBuilder[] strings = new StringBuilder[numRows];
+        for (int j = 0; j < strings.length; j++) {
+            strings[j] = new StringBuilder();
+        }
+        for (char c : s.toCharArray()){
+            strings[indexLetter].append(c);
+            if (indexLetter == 0 || indexLetter == numRows - 1)
+                b = !b;
+            indexLetter += b ? 1 : -1;
+        }
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder string : strings) {
+            result.append(string);
+        }
+        return result.toString();
+    }
+}
