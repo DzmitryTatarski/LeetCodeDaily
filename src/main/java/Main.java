8b8813072d6031d09abdbd559c0e77211a52ca8d
tatar <tatarrski_dmitry@mail.ru>
2023-07-10 16:24:39 +0300

    task 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import task9.Solution;
+import task10.Solution;
 
 // the class to check results
 public class Main {
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(Integer.MAX_VALUE);
-        System.out.println(solution.isPalindrome(10));
+        System.out.println(solution.isMatch("aaaaaaaaaaaaaaa", ".*"));
 
     }
 
