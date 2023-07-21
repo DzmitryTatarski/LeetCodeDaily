107332c69cc83bacdedcbaca1467434d0b460ee8
tatar <tatarrski_dmitry@mail.ru>
2023-07-21 12:53:34 +0300

    task 17

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
-import task1x.task16.Solution;
+import task1x.task17.Solution;
 
 // the class to check results
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.threeSumClosest(new int[]{0,0,0}, 1));
+        System.out.println(solution.letterCombinations("23"));
 
     }
 
